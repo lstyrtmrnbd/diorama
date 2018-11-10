b85bb44fc3a191132078c30577a073e0c17e2835
Inaba Tewi <soymilk@airmail.cc>
2018-11-10 12:50:32 -0500

    begin texturing

--- diorama.cpp
+++ diorama.cpp
@@ -9,35 +9,55 @@
 
 #include <glm/glm.hpp>
 
-int main() {
+using glm::vec2;
+using glm::vec3;
+using std::string;
 
-  using glm::vec3;
-  using std::string;
+int main() {
 
   sf::ContextSettings settings(24, 0, 0, 4, 5); //depth, stencil, AA, major, minor
   
   sf::Window window(sf::VideoMode(800, 600), "Diorama", sf::Style::Default, settings);
   window.setVerticalSyncEnabled(true);
 
-  // activate the window
   window.setActive(true);
 
-  // load resources, initialize the OpenGL states, ...
+  // load resources, initialize the OpenGL states...
 
   glewInit();
 
   glClearColor(0.5f,0.5f,0.5f,1.0f);
+  glEnable(GL_DEPTH_TEST);
 
-  vec3 vertices[3];
-  vertices[0] = vec3(0.5f,0.5f,0.0f);
-  vertices[1] = vec3(0.0f,0.0f,0.0f);
-  vertices[2] = vec3(1.0f,0.0f,0.0f);
+  // By default, counterclockwise polygons are taken to be front-facing
+  vec3 vertices[6];
+  vertices[0] = vec3(-0.5f, 1.0f,0.0f);
+  vertices[1] = vec3(-0.5f,-1.0f,0.0f);
+  vertices[2] = vec3( 0.5f,-1.0f,0.0f);
+
+  vertices[3] = vec3( 0.5f,-1.0f,0.0f);
+  vertices[4] = vec3( 0.5f, 1.0f,0.0f);
+  vertices[5] = vec3(-0.5f, 1.0f,0.0f);
   
   GLuint VBO;
   glGenBuffers(1, &VBO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+  vec2 texCoords[6];
+  texCoords[0] = vec2( 0.0f, 1.0f);
+  texCoords[1] = vec2( 0.0f, 0.0f);
+  texCoords[2] = vec2( 1.0f, 0.0f);
+
+  texCoords[3] = vec2( 1.0f, 0.0f);
+  texCoords[4] = vec2( 1.0f, 1.0f);
+  texCoords[5] = vec2( 0.0f, 1.0f);
+
+  GLuint texCoordBuffer;
+  glGenBuffers(1, &texCoordBuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, texCoordBuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(texCoords), texCoords, GL_STATIC_DRAW);
+  
   const string vertSrc = "in vec3 position;"\
                          "varying vec2 texCoord;"\
                          "void main() {"\
@@ -45,20 +65,31 @@ int main() {
                          "}";
 
   const string fragSrc = "varying vec2 texCoord;"\
-                         "in sampler2D texture;"\
+                         "uniform sampler2D texture0;"\
                          "void main() {"\
-                         "  gl_FragColor = vec4(0.0,1.0,0.5,1.0);"\
+                         "  vec3 tex = texture2D(texture0, texCoord).rgb;"\
+                         "  vec3 color = vec3(0.0, 1.0, 0.5);"\
+                         "  gl_FragColor = vec4(color / tex,1.0);"\
                          "}";
 
   sf::Shader defaultShader;
 
   if (!defaultShader.loadFromMemory(vertSrc, fragSrc)) std::cout << "#####Default shader failed\n";
 
+  GLuint shaderHandle = defaultShader.getNativeHandle();
+
+  GLint positionLoc = glGetAttribLocation(shaderHandle, "position");
+  GLint texCoordLoc = glGetAttribLocation(shaderHandle, "texCoord");
+
+  
   sf::Image defaultImage;
   defaultImage.loadFromFile("poster0.jpg");
 
   sf::Texture defaultTexture;
   defaultTexture.loadFromImage(defaultImage);
+
+  GLuint texHandle = defaultTexture.getNativeHandle();
+
   
   bool running = true;
   while (running) {
@@ -93,14 +124,19 @@ int main() {
     sf::Shader::bind(&defaultShader);
 
     sf::Texture::bind(&defaultTexture);
-
-    glEnableVertexAttribArray(0);   
+    
+    glEnableVertexAttribArray(positionLoc);
+    glEnableVertexAttribArray(texCoordLoc);
+    
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0); //tell pipeline how to interpret data in the buffer
+    glVertexAttribPointer(positionLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-    glDrawArrays(GL_TRIANGLES, 0 , 3);
+    glBindBuffer(GL_ARRAY_BUFFER, texCoordBuffer);
+    glVertexAttribPointer(texCoordLoc, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
-    glDisableVertexAttribArray(0);
+    glDrawArrays(GL_TRIANGLES, 0 , 6);
+
+    //glDisableVertexAttribArray(positionLoc);
     
     window.display(); // end the current frame (internally swaps the front and back buffers)
   }
